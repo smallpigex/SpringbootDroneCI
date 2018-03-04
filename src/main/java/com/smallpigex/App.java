a16a19be878d1258c4a1d7abcc4b3f917bc28225
hwchu <smallpigex@gmail.com>
2018-03-04 11:26:35 +0800

    add drone file

--- src/main/java/com/smallpigex/App.java
+++ src/main/java/com/smallpigex/App.java
@@ -4,8 +4,7 @@ package com.smallpigex;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
